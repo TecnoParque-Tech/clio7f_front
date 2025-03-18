495617b8e750d7ef85f3e0cb07be66c294052521
Santiago Leon <93327269+Leonslp@users.noreply.github.com>
2025-03-18 16:07:47 -0500

    .

--- src/containers/ToolTeam.jsx
+++ src/containers/ToolTeam.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import styled from 'styled-components';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { SurveyContext } from '../SurveyContext';
 
 const questions = [
